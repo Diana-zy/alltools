d1beb47034969ed24f4b76f38d78373d0f9c5481
MingjiaChen <chenmingjia13@gmail.com>
2024-06-11 13:38:57 +0800

    feat: 首页接口

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@
       <h2 class="title-h2">Best Apps</h2>
       <section class="box-row-scroll">
         <ContentItemRow
-          v-for="(item, index) in bestGames"
+          v-for="(item, index) in bestApps"
           :key="index"
           :item="item"
           :index="index"
@@ -65,55 +65,44 @@ export default {
   async asyncData({ $axios, env }) {
     try {
       // 并行处理多个异步请求
-      const [
-        recGamesResponse,
-        bestGamesResponse,
-        newGamesResponse,
-        hotGamesResponse,
-        allCategoriesResponse
-      ] = await Promise.all([
-        $axios.$get("/api/game/menu", {
-          params: {
-            site_id: env.SITE_ID,
-            mod_id: "rec-games",
-            size: 3
-          }
-        }),
-        $axios.$get("/api/game/menu", {
-          params: {
-            site_id: env.SITE_ID,
-            mod_id: "best-games",
-            size: 12
-          }
-        }),
-        $axios.$get("/api/game/menu", {
-          params: {
-            site_id: env.SITE_ID,
-            mod_id: "new-games",
-            size: 14
-          }
-        }),
-        $axios.$get("/api/game/menu", {
-          params: {
-            site_id: env.SITE_ID,
-            mod_id: "hot-games",
-            size: 36
-          }
-        }),
-        $axios.$get("/api/game/get_all_category", {
-          params: {
-            site_id: env.SITE_ID
-          }
-        })
-      ]);
+      const [bestAppsResponse, bestGamesResponse, hotAppsResponse, hotGamesResponse] =
+        await Promise.all([
+          $axios.$get("/api/game/menu", {
+            params: {
+              site_id: env.SITE_ID,
+              mod_id: "best-apps",
+              size: 12
+            }
+          }),
+          $axios.$get("/api/game/menu", {
+            params: {
+              site_id: env.SITE_ID,
+              mod_id: "best-games",
+              size: 12
+            }
+          }),
+          $axios.$get("/api/game/menu", {
+            params: {
+              site_id: env.SITE_ID,
+              mod_id: "hot-apps",
+              size: 12
+            }
+          }),
+          $axios.$get("/api/game/menu", {
+            params: {
+              site_id: env.SITE_ID,
+              mod_id: "hot-games",
+              size: 12
+            }
+          })
+        ]);
 
       // 返回多个接口的数据
       return {
-        recGames: recGamesResponse.list,
+        bestApps: bestAppsResponse.list,
         bestGames: bestGamesResponse.list,
-        newGames: newGamesResponse.list,
-        hotGames: hotGamesResponse.list,
-        navCategories: allCategoriesResponse.list.slice(0, 8)
+        hotApps: hotAppsResponse.list,
+        hotGames: hotGamesResponse.list
       };
     } catch (error) {
       console.error("Error fetching data:", error);
