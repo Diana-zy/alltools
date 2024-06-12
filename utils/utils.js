7364c52d671de8688a637566a817551ec955621d
MingjiaChen <chenmingjia13@gmail.com>
2024-06-12 12:09:30 +0800

    生成跳转链接

--- utils/utils.js
+++ utils/utils.js
@@ -46,6 +46,7 @@ export function generateCustomLink(url) {
     { pattern: /^\/category\/[\w-]+\/$/, type: "category-detail" },
     { pattern: /^\/category\/$/, type: "category" },
     { pattern: /^\/game\/.*/, type: "game" },
+    { pattern: /^\/app\/.*/, type: "app" },
     { pattern: /^\/download\/.*/, type: "download" },
     { pattern: /^\/$/, type: "home" }
   ];
@@ -60,31 +61,37 @@ export function generateCustomLink(url) {
     currentParams.delete("text");
   }
 
-  const siteId = process.env.SITE_ID;
   const currentDomain = window.location.host;
+
   let targetDomain = "";
 
-  // 检查当前域名是否为 x.siteId.com 的情况
-  // if (new RegExp(`^[0-9]+\\.${siteId}\\.com$`).test(currentDomain)) {
-  //   targetDomain = "www" + currentDomain;
-  // }
-  // // 检查当前域名是否为 siteId.com 的情况
-  // else
-  if (currentDomain === `${siteId}.com`) {
-    targetDomain = "www." + currentDomain;
-  }
-  // 检查当前域名是否为 www.siteId.com 的情况
-  else if (currentDomain === `www.${siteId}.com`) {
-    targetDomain = currentDomain.substring(4);
-  }
-  // 检查当前域名是否为 wwwx.siteId.com 的情况
-  // else if (new RegExp(`^www[0-9]*\\.${siteId}\\.com$`).test(currentDomain)) {
-  //   targetDomain = currentDomain.substring(3);
-  // }
-  // 如果都不符合，那么当前域名可能是其他非预期的情况
-  else {
+  // 提取主域名和顶级域名部分的正则表达式
+  // eslint-disable-next-line no-useless-escape
+  const domainPattern = /^(www[0-9]*|[0-9]*)\.?([^\.]+)\.([a-z]{2,})$/;
+  const match = currentDomain.match(domainPattern);
+  if (match) {
+    const prefix = match[1]; // 提取前缀部分，如 "www", "www2", "2", ""
+
+    if (!prefix) {
+      // case: siteId.tld
+      targetDomain = `www.${currentDomain}`;
+    } else if (prefix.startsWith("www")) {
+      if (prefix === "www") {
+        // case: www.siteId.tld
+        targetDomain = currentDomain.substring(4);
+      } else {
+        // case: wwwx.siteId.tld
+        targetDomain = currentDomain.substring(3);
+      }
+    } else {
+      // case: x.siteId.tld
+      targetDomain = `www${currentDomain}`;
+    }
+  } else {
+    // 如果不匹配期望的模式，保留当前域名
     targetDomain = currentDomain;
   }
+
   const protocol = window.location.protocol;
 
   // 生成新的查询参数字符串
